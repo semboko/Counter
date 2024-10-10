fbe9b4964707046aaae214f5706a79b0024d9262
Simon Bondarenko <bondarenko@vt.edu>
2024-10-10 10:58:33 -0600

    Oct 9

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+import { memo, useMemo, useState } from 'react';
+
+const HeavyComponent = memo((props: {usernames: string[]}) => {
+  console.log("Expensive computations...")
+  return (
+    <div>Hello, {props.usernames.length}</div>
+  )
+})
+
 function App() {
+  const [counter, setCounter] = useState<number>(0)
+  const usernames = useMemo(() => ["a", "b", "c"], [])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <HeavyComponent usernames={usernames} />
+      <button onClick={() => setCounter(counter + 1)}>{counter}</button>
     </div>
   );
 }
